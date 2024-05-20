2c7c8788a9bc35dc75b4fee452fc23adb85c4655
Kabir <dexkode4@gmail.com>
2024-05-20 13:07:12 +0100

    update

--- utils.js
+++ utils.js
@@ -1,12 +1,15 @@
-
 // const { greet } = require('https://cdn.jsdelivr.net/gh/dexkode4/my-module/index.js');
 // greet()
 
 export const greet = () => {
-    console.log("hello");
-  };
-  
-  export const shout = (msg) => {
-    console.log(msg);
-  };
-  
+  console.log("hello");
+};
+
+export const shout = (msg) => {
+  console.log(msg);
+};
+
+module.exports = {
+  shout,
+  greet,
+};
