c53230ed5cf391e9fe2f89d83b1314ca297e986f
Kabir <dexkode4@gmail.com>
2024-06-05 10:07:21 +0100

    update webpack

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    library: 'MyModule',
+    libraryTarget: 'var'
+  },
+  mode: 'development'
+};
